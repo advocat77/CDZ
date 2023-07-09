279519750a34ae67f12fc2f109d781787871d4de
Alex <advocat6787428@gmail.com>
2023-07-09 14:36:38 +0200

    task2 added

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+int max = number1;
+if (max > number2)
+{
+   max = number1;
+}
+else
+   if (number3 > max)
+{
+   max = number3;
+}
+Console.WriteLine($"Число {max} является максимальным");
+
+
