e5db016f3b32ab7efea70cdda42d2bd46b6ee8f3
Alex <advocat6787428@gmail.com>
2023-07-09 21:29:18 +0200

    task4 added

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if (number > 1)
+{
+   while (evenNumber <= number)
+   {
+      Console.Write(evenNumber + " ");
+      evenNumber = evenNumber + 2;
+   }
+}
+
+
+
+
+
